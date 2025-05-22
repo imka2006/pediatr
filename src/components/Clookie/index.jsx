367f4c10a4ecd9a8a381b2adb7439e6de902ac8e
imka2006 <31unknown12@gmail.com>
2025-05-22 23:15:42 +0600

    йцукер

--- src/components/Clookie/index.jsx
+++ src/components/Clookie/index.jsx
@@ -7,14 +7,20 @@ import "./style.scss";
 function Cookie() {
     const [lil, setLil] = useState(true);
     const [full, setFull] = useState(false);
-
+    const onClose = () => {
+        setFull(false);
+        setLil(false);
+    };
     return (
         <>
             {lil && <Lil setLil={setLil} setFull={setFull} />}
             {full && (
                 <>
-                    <div className="cookie-wrapper" onClick={() =>setFull(false) }></div>
-                    <Full  setLil={setLil} setFull={setFull}/>
+                    <div
+                        className="cookie-wrapper"
+                        onClick={() => onClose()}
+                    ></div>
+                    <Full setLil={setLil} setFull={setFull} />
                 </>
             )}
         </>
